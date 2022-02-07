1a4f34a1170384b0f0b0800faa7acfd017d01d80
Alexis Fourquet <afourquet@oxibis.com>
2022-02-07 15:12:24 +0100

    Init depot

---
+++ doc/snippet_data.js
@@ -0,0 +1,46 @@
+
+var faq = [
+  { id: 'faq1', text: "Ici une ligne centrée", icon: "geolo.svg" },
+  { id: 'faq2', text: "Ici 2 lignes... et sinon,", text2: "vous aimez le fromage ?", icon: "lunettes.svg" },
+  { id: 'faq3', text: "Comté, Morbier, Raclette...", text2: "ça donne faim hein ?", icon: "news.svg" },
+  { id: 'faq4', text: "Allons restons concentré,", text2: "c'est sérieux tout de même :)", icon: "mon compte.svg" },
+  { id: 'faq5', text: "... On vous attendra pour la fondue ;)", icon: null }
+];
+
+
+var menu_footer = [
+  {
+    icon: 'lunettes footer.png',
+    submenu: [
+      { id: 'ml1', text: "Donc, on est dans la lunette", icon: null },
+      { id: 'ml2', text: "Vous en portez ?", icon: null },
+      { id: 'ml3', text: "Attention à ce qui suit :", icon: null },
+      { id: 'ml4', text: "c'est en capitale italic", class: 'italicCap', icon: null }
+    ]
+  },
+  {
+    icon: 'bulle.png',
+    submenu: [
+      { id: 'mb1', text: "Contact", icon: null },
+      { id: 'mb2', text: "Blah blah", icon: null },
+      { id: 'mb3', text: "bref on taille une bavette", icon: null }
+    ]
+  },
+  {
+    icon: 'connexion.png',
+    submenu: [
+      { id: 'mc1', text: "Ici les zones lockées", icon: null },
+      { id: 'mc2', text: "Genre login", icon: null },
+      { id: 'mc3', text: "Et mot de passe", icon: null }
+    ]
+  },
+];
+
+
+var menuHover = [
+  { id: 'mH1', text: "imaginons un menu...", icon: null },
+  { id: 'mH2', text: "un onglet", icon: null },
+  { id: 'mH3', text: "attention italic et rouge", class: 'menuAttention', icon: null },
+  { id: 'mH4', text: "et la il y a un fond gris", class: 'menuFG', icon: null },
+  { id: 'mH5', text: "mais ca va bien se passer !", class: 'menuFG', icon: null },
+];
